45ccd63bf83f269cce751e3c4fad28424a267b41
EvgeniyGlubokiy <eglubokiy@gmail.com>
2020-10-01 22:14:05 +0300

    another commit

---
+++ JS/main.js
@@ -0,0 +1,113 @@
+///=====================SLIDER
+$(function () {
+    $(".header-block_slider").slick({
+        dots: false,
+        slidesToShow: 1,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 4000,
+        arrows: true,
+        fade: true,
+        speed: 1500,
+        responsive: [
+            {
+                breakpoint: 768.98,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: true,
+
+                },
+            },
+        ],
+    });
+});
+
+
+///===============SCROLL MENU
+
+$(() => {
+    $(document).scroll(() => {
+        if ($(window).scrollTop() > 50) {
+            $(".top-nav").addClass("top-nav__scroll");
+
+        } else {
+            $(".top-nav").removeClass("top-nav__scroll");
+
+        }
+
+    });
+
+});
+//============== HOVER button (TEST)
+// $(document).ready(function () {
+//     $('.btn').hover(function () {
+//         $('.cost-card').css({ 'background-color': '#000000', 'transition': '0.5s', 'color': '#ffffff' });
+//         $('.cost-card__list-item').css({ 'border-color': '#ffffff' });
+//     })
+//     $('.btn').mouseout(function () {
+//         $('.cost-card').css({ 'background-color': '#ffffff', 'transition': '0.5s', 'color': '#000000' });
+//         $('.cost-card__list-item').css({ 'border-color': '#000000' });
+//     })
+// })
+//=======================Send Form
+$(document).ready(function () {
+    $('#form').submit(function () {
+        $.ajax({
+            type: "POST",
+            url: "form.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $('.js-overlay-thank-you').fadeIn();
+            $(this).find('input').val('');
+            $('#form').trigger('reset');
+        });
+        return false;
+    });
+});
+////////ARROW UP
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $(".btn-top").fadeIn();
+        } else {
+            $(".btn-top").fadeOut();
+        }
+    });
+    $(".btn-top").click(function () {
+        $("body,html").animate({ scrollTop: 0 }, 900);
+    });
+});
+
+///SWING url
+
+$(document).ready(function () {
+    $(".swing, a").click(function () {
+        $("html, body").animate(
+            {
+                scrollTop: $($(this).attr("href")).offset().top + "px",
+            },
+            {
+                duration: 750,
+                easing: "linear",
+            }
+        );
+        return false;
+    });
+});
+
+
+
+
+
+///по началу блока
+// $(document).ready(function () {
+//     let headerHeight = $('header').outerHight();
+//     $('swing').click(function (e) {
+//         let linkHref = $(this).attr('href');
+//         $('html,body').animate({
+//             scrollTop: $(linkHref).offset().top - headerHeight
+//         }, 1000);
+//         e.preventDefault();
+//     });
+// });
---
+++ css/form.php
@@ -0,0 +1,14 @@
+<?php
+$recepient = "eglubokiy@gmail.com";
+$siteName = "evgeniy2020.pp.ua";
+$name = trim($_POST["name"]);
+$mail = trim($_POST["mail"]);
+$phone = trim($_POST["phone"]);
+$messageText = trim($_POST["message"]);
+
+$message = "Имя клиента: $name \nE-mail: $mail \nТелефон: $phone \nСообщение: $messageText";
+
+$pageTitle = "Заявка с сайта \"$siteName\"";
+
+mail($recepient, $pageTitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
